ce93c197aa4a090c9bb229b5444438f4870d76ce
aminoqr <aminoqr12@gmail.com>
2026-02-17 20:12:03 +0100

    ex1: Add SpellCard class with play and resolve_effect methods

---
+++ Python Module 07/ex1/SpellCard.py
@@ -0,0 +1,18 @@
+from ex0.Card import Card
+
+
+class SpellCard(Card):
+    def __init__(self, name: str, cost: int, rarity: str,
+                 effect_description: str) -> None:
+        super().__init__(name, cost, rarity)
+        self.effect_description = effect_description
+
+    def play(self, game_state: dict) -> dict:
+        return {
+            "card_played": self.name,
+            "mana_used": self.cost,
+            "effect": self.effect_description
+        }
+
+    def resolve_Effect(self, targets: list) -> dict:
+        ...
