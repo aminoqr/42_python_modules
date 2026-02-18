faec01e72a9f08a3fb9bdd0b22649588dc87f8dc
aminoqr <aminoqr12@gmail.com>
2026-02-18 18:32:57 +0100

    ex1: Fix SpellCard.py flake8 compliance

--- Python Module 07/ex1/SpellCard.py
+++ Python Module 07/ex1/SpellCard.py
@@ -2,16 +2,18 @@ from ex0.Card import Card
 
 
 class SpellCard(Card):
+    card_type = "Spell"
+
     def __init__(self, name: str, cost: int, rarity: str,
-                 effect_description: str) -> None:
+                 effect_type: str) -> None:
         super().__init__(name, cost, rarity)
-        self.effect_description = effect_description
+        self.effect_type = effect_type
 
     def play(self, game_state: dict) -> dict:
         return {
             "card_played": self.name,
             "mana_used": self.cost,
-            "effect": self.effect_description
+            "effect": self.effect_type
         }
 
     def resolve_Effect(self, targets: list) -> dict:
