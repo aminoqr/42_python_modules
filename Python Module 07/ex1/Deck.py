c537bdab866295a9ec9e41967918eef514eaf376
aminoqr <aminoqr12@gmail.com>
2026-02-18 18:32:54 +0100

    ex1: Fix Deck.py flake8 compliance

--- Python Module 07/ex1/Deck.py
+++ Python Module 07/ex1/Deck.py
@@ -5,21 +5,22 @@ from ex1.SpellCard import SpellCard
 from typing import List, Dict
 from random import shuffle
 
+
 class Deck:
     def __init__(self,) -> None:
         self.cards: List[Card] = []
-    
+
     def add_card(self, card: Card) -> None:
         self.cards.append(card)
-    
+
     def shuffle(self) -> None:
         shuffle(self.cards)
-    
+
     def draw_card(self) -> Card | None:
         if not self.cards:
             return None
         return self.cards.pop()
-    
+
     def get_deck_stats(self) -> Dict:
         cc_count = 0
         ac_count = 0
@@ -33,20 +34,19 @@ class Deck:
             elif isinstance(i, SpellCard):
                 sc_count += 1
             total_cost += i.cost
-        
+
         avg = total_cost/len(self.cards) if self.cards else 0.0
         return {
             "total_cards": len(self.cards),
             "creatures": cc_count,
             "spells": sc_count,
             "artifacts": ac_count,
-            "avg_cost": avg
+            "avg_cost": round(avg, 2)
         }
-    
+
     def remove_card(self, card_name: str) -> bool:
         for card in self.cards:
             if card.name == card_name:
                 self.cards.remove(card)
                 return True
         return False
-            
